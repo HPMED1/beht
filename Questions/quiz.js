c1c06b6f15c4b3f4ae6eb22f6e2b45e5ea0b9554
HPMED <62830536+HPMED1@users.noreply.github.com>
2024-06-16 01:01:37 +0300

    Update quiz.js

--- Questions/quiz.js
+++ Questions/quiz.js
@@ -152,7 +152,7 @@ function toggleActive(){
 function next(){
 
     if(question_count == questions.length -1){
-        location.href = "\/Result";
+        location.href = "/Result";
     }
     console.log(question_count);
 
